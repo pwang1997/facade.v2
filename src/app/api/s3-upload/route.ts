21760248b28380f76da6515233c7ac7813a1dc65
Zhengliang Wang <puck19970418@gmail.com>
2024-06-26 12:29:37 -0400

    add s3 upload feat

---
+++ src/app/api/s3-upload/route.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { NextResponse } from "next/server";
+import { env } from "~/env";
+
+const s3Client = new S3Client({
+  region: env.AWS_S3_REGION,
+  credentials: {
+    accessKeyId: env.AWS_S3_ACCESS_KEY_ID,
+    secretAccessKey: env.AWS_S3_SECRET_ACCESS_KEY,
+  },
+});
+
+async function uploadFileToS3(file: Buffer, fileName: any) {
+  const fileBuffer = file;
+  console.log(fileName);
+
+  const params = {
+    Bucket: env.AWS_S3_BUCKET_NAME,
+    Key: `${fileName}`,
+    Body: fileBuffer,
+    ContentType: "image/jpg",
+  };
+
+  const command = new PutObjectCommand(params);
+  await s3Client.send(command);
+  return fileName;
+}
+
+export async function POST(request: { formData: () => any; }) {
+  try {
+    const formData = await request.formData();
+    const file = formData.get("file");
+    
+
+    if (!file) {
+      return NextResponse.json({ error: "File is required." }, { status: 400 });
+    }
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const fileName = await uploadFileToS3(buffer, file.name);
+
+    return NextResponse.json({ success: true, fileName });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
