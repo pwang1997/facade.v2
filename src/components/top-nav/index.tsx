589a5e7d4383828724b935118609c573faa32c4d
Zhengliang Wang <puck19970418@gmail.com>
2024-06-25 22:18:58 -0400

    update top nav with nested categories

--- src/components/top-nav/index.tsx
+++ src/components/top-nav/index.tsx
@@ -1,14 +1,23 @@
 
+import { type Category } from "~/app/admin/categories/_components/data-table";
 import { api } from "~/trpc/server";
 import NavMenu from "./nav-menu";
+
+export type NestedCategory = {
+    children : Category[]
+} & Category;
 
 export default async function TopNav() {
     const categories = await api.category.list({ offset: 0, limit: 1000 });
     const topCategories = categories.filter(category => category.parentId === null);
-    const categoryNames = topCategories.map((item) => item.name);
 
+    const nestedCategories : NestedCategory[]  = [];
+    topCategories.forEach(category => {
+        const childCategories = categories.filter(item => item.parentId === category.id);
+        nestedCategories.push({...category, children : childCategories});
+    })
 
     return (
-        <NavMenu categories={categoryNames} />
+        <NavMenu categories={nestedCategories} />
     )
 }
--- src/components/top-nav/nav-menu.tsx
+++ src/components/top-nav/nav-menu.tsx
@@ -1,18 +1,58 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import CommandLineIcon from "~/icons/CommandLineIcon";
 import GitHubIcon from "~/icons/GitHubIcon";
+import { type NestedCategory } from ".";
 import { CommandLine } from "../command-line";
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "../ui/navigation-menu";
+import { ListItem, NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "../ui/navigation-menu";
 
 const defaultRoutes = [
     { label: "posts", href: "posts" },
     { label: "about", href: "about" },
     { label: "dev notes", href: "developer-notes" }
 ]
-export default function NavMenu({ categories }: { categories: string[] }) {
+
+const components: { title: string; href: string; description: string }[] = [
+    {
+        title: "Alert Dialog",
+        href: "/docs/primitives/alert-dialog",
+        description:
+            "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+        title: "Hover Card",
+        href: "/docs/primitives/hover-card",
+        description:
+            "For sighted users to preview content available behind a link.",
+    },
+    {
+        title: "Progress",
+        href: "/docs/primitives/progress",
+        description:
+            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+    },
+    {
+        title: "Scroll-area",
+        href: "/docs/primitives/scroll-area",
+        description: "Visually or semantically separates content.",
+    },
+    {
+        title: "Tabs",
+        href: "/docs/primitives/tabs",
+        description:
+            "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+    },
+    {
+        title: "Tooltip",
+        href: "/docs/primitives/tooltip",
+        description:
+            "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+    },
+]
+
+export default function NavMenu({ categories, }: { categories: NestedCategory[] }) {
     const paths = usePathname();
 
     const [show, setShow] = useState<boolean>(false);
@@ -36,15 +76,32 @@ export default function NavMenu({ categories }: { categories: string[] }) {
                         </NavigationMenuItem>
                         {
                             categories.map((category) => {
-                                return (
-                                    <Fragment key={category}>
-                                        <NavigationMenuItem>
-                                            <NavigationMenuLink href={`/categories/${category}`} className={navigationMenuTriggerStyle()}>
-                                                {category}
+                                if (category?.children?.length === 0) {
+                                    return (
+                                        <NavigationMenuItem key={category.id}>
+                                            <NavigationMenuLink href={`/categories/${category.name}`} className={navigationMenuTriggerStyle()}>
+                                                {category.name}
                                             </NavigationMenuLink>
                                         </NavigationMenuItem>
-                                    </Fragment>
-                                )
+                                    )
+                                } else {
+                                    return (
+                                        <NavigationMenuItem key={category.id}>
+                                            <NavigationMenuTrigger>{category.name}</NavigationMenuTrigger>
+                                            <NavigationMenuContent>
+                                                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                                                    {category.children.map((component) => (
+                                                        <ListItem
+                                                            key={component.id}
+                                                            title={component.name}
+                                                            href={`/categories/${component.name}`}
+                                                        />
+                                                    ))}
+                                                </ul>
+                                            </NavigationMenuContent>
+                                        </NavigationMenuItem>
+                                    )
+                                }
                             })
                         }
 
